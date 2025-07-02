d014ab367a87350c74c9ee4f44ae3d9616f1bcb2
unknown <caldayham@gmail.com>
2025-07-02 01:12:31 -0400

    responsive

--- components/About_Components/HookBanner.tsx
+++ components/About_Components/HookBanner.tsx
@@ -6,8 +6,8 @@ import CTAButton from '../CTAButton';
 
 const HookBanner = () => {
   return (
-    <div className="min-h-screen px-4 md:px-12 bg-white bg-grid flex items-center">
-      <div className="max-w-6xl mx-auto pb-20 flex flex-col md:flex-row items-center gap-5 pt-0 md:pt-0">
+    <div className="min-h-[100dvh] px-4 md:px-12 bg-white bg-grid flex items-center">
+      <div className="max-w-6xl mx-auto pb-20 flex flex-col md:flex-row items-center gap-5 pt-10 md:pt-0">
 
         {/* Left: Text */}
         <div className="w-full md:basis-1/2 lg:basis-[60%] text-center md:text-left">
